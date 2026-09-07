fb64ff02404c742f12f4c2c8a98b9fb11ad2e141
mallapudileep <mallapudileep36@gmail.com>
2026-09-07 19:52:52 +0530

    updated the s

--- film/settings.py
+++ film/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGIN=['http://localhost:5173/','https://movie-frontend-topaz.vercel.app/']
+CORS_ALLOWED_ORIGINS=['http://localhost:5173/','https://movie-frontend-topaz.vercel.app/']
